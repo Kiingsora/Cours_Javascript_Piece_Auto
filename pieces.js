7fc6863c0ac1792c9879f34345048601a30f06c2
kiingsora <hichem.m83@gmail.com>
2026-02-11 15:07:32 +0100

    exercice générer du contenu de votre page grâce au DOM terminé + affichage de la liste via boucle foreach

--- pieces.js
+++ pieces.js
@@ -1,5 +1,7 @@
 // VARIABLES
 
+// reprendre à vérifier les données
+
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
@@ -10,23 +12,37 @@ console.log(pieces);
 const article = pieces[0];
 console.log(article);
 
+const articles = pieces;
+
 const fiches = document.querySelector(".fiches");
+articles.forEach((article) => {
+  let image = document.createElement("img");
+  let nomPiece = document.createElement("h2");
+  let prixArticle = document.createElement("p");
+  let categorie = document.createElement("p");
+  let description = document.createElement("p");
+  let disponible = document.createElement("p");
 
-const image = document.createElement("img");
-const nomPiece = document.createElement("h2");
-const prixArticle = document.createElement("p");
-const categorie = document.createElement("p");
+  image.src = article.image;
+  nomPiece.innerText = article.nom;
 
+  prixArticle.innerText = ` Prix : ${article.prix}€ (${
+    article.prix < 35 ? "€" : "€€€"
+  }) `;
 
-image.src = article.image;
-nomPiece.innerText = article.nom;
-prixArticle.innerText = ` Prix : ${article.prix}E `;
-categorie.innerText = article.categorie;
+  categorie.innerText = article.categorie ?? "(aucune catégorie)";
 
-fiches.appendChild(image)
-fiches.appendChild(nomPiece)
-fiches.appendChild(prixArticle)
-fiches.appendChild(categorie)
+  description.innerText = `${article.description ?? "Pas de description pour le moment."
+  }`;
+
+  disponible.innerText = `${article.disponible ? "En stock": "Rupture de stock"}`
+  fiches.appendChild(image);
+  fiches.appendChild(nomPiece);
+  fiches.appendChild(prixArticle);
+  fiches.appendChild(categorie);
+  fiches.appendChild(description);
+  fiches.appendChild(disponible)
+});
 
 // FONCTIONS
 
