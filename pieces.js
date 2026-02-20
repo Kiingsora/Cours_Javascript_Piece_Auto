1bf4916110944ab3abb3019e32b9abb1f7ee763e
kiingsora <hichem.m83@gmail.com>
2026-02-20 17:43:11 +0100

    ajout des boutons pour trier en ordre croissant/ décroissant + affichage des articles sans description + les articles les moins cher

--- pieces.js
+++ pieces.js
@@ -4,13 +4,17 @@ const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
 const article = pieces[0];
-
 const articles = pieces;
 
-for (let i = 0; i < articles.length; i++) {
+const btnCroissant = document.querySelector(".btn-trier");
+const btnFiltrer = document.querySelector(".btn-filtrer");
+const btnDecroissant = document.querySelector(".btn-decroissant");
+const btnSansDescription = document.querySelector(".btn-sansDescription");
+const btnDescription = document.querySelector(".btn-description");
 
+for (let i = 0; i < articles.length; i++) {
   const fiches = document.querySelector(".fiches");
-  let baliseArticle = document.createElement("article")
+  let baliseArticle = document.createElement("article");
   let image = document.createElement("img");
   let nomPiece = document.createElement("h2");
   let prixArticle = document.createElement("p");
@@ -35,17 +39,59 @@ for (let i = 0; i < articles.length; i++) {
     articles[i].disponible ? "En stock" : "Rupture de stock"
   }`;
 
-  fiches.appendChild(baliseArticle)
+  fiches.appendChild(baliseArticle);
   baliseArticle.appendChild(image);
   baliseArticle.appendChild(nomPiece);
   baliseArticle.appendChild(prixArticle);
   baliseArticle.appendChild(categorie);
   baliseArticle.appendChild(description);
   baliseArticle.appendChild(disponible);
-  console.log(fiches);
 }
-
 
 // FONCTIONS
 
 // EVENT
+
+btnCroissant.addEventListener("click", function () {
+  const pieceOrdonees = Array.from(pieces);
+
+  pieceOrdonees.sort(function (a, b) {
+    return a.prix - b.prix;
+  });
+
+  console.log(pieceOrdonees);
+});
+
+btnDecroissant.addEventListener("click", function () {
+  const pieceNonOrdonees = Array.from(pieces);
+
+  pieceNonOrdonees.sort(function (a, b) {
+    return b.prix - a.prix;
+  });
+
+  console.log(pieceNonOrdonees);
+});
+
+btnFiltrer.addEventListener("click", function () {
+  const piecesFiltrees = pieces.filter(function (piece) {
+    return piece.prix <= 35;
+  });
+  console.log(piecesFiltrees);
+});
+
+btnSansDescription.addEventListener("click", function () {
+  const pieceSansDescription = pieces.filter(
+    (piece) => !("description" in piece)
+  );
+  console.log(pieceSansDescription);
+
+  return pieceSansDescription;
+});
+
+btnDescription.addEventListener("click", function () {
+
+  const pieceDescription = pieces.filter((piece) => "description");
+  console.log(pieceDescription);
+
+  return pieceDescription;
+});
