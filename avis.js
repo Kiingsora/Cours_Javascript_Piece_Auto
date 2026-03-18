6cac0bd4d516c755f445cb8b5b937a12c859f9fd
kiingsora <hichem.m83@gmail.com>
2026-03-18 12:47:17 +0100

    ajout de commentaire qui s'affiche lors du clique sur le bouton

--- avis.js
+++ avis.js
@@ -1,15 +1,19 @@
 export function ajoutListenersAvis() {
-    const piecesElements = document.querySelectorAll(".fiches article button");
-  
-    for (let i = 0; i < piecesElements.length; i++) {
-      piecesElements[i].addEventListener("click", async function (event) {
-
+    const btnAvis = document.querySelectorAll(".fiches article button");
+    for (let i = 0; i < btnAvis.length; i++) {
+      btnAvis[i].addEventListener("click", async function (event) {
+        
         const id = event.target.dataset.id;   
         const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
-        const avis = await reponse.json()
-        console.log(avis);    
-         
+        const avis = await reponse.json();
+
+        const avisElement = document.createElement("p");
+        const piecesElements = event.target.parentElement;
+        piecesElements.appendChild(avisElement);
+
+        for (let i = 0; i < avis.length; i++) {
+          avisElement.innerHTML += `${avis[i].utilisateur} : ${avis[i].commentaire} <br>`          
+        }
       });
     }
-    
   }
